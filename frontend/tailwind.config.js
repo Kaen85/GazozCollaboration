8fac3352a63b87f841079f7ba2a20fe613861091
Kaan200535 <kaan.kavas@final.edu.tr>
2025-12-21 19:52:54 +0200

    backup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: [],
+  darkMode: true, // or 'media' or 'class'
+  
+  theme: {
+    extend: {},
+  },
+  
+  variants: {
+    extend: {},
+  },
+  
+  plugins: [],
+}
